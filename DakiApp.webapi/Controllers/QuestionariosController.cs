f0da846b7fa2435abad148037c87bf20a8690ed4
Fabio Freller <f.freller@gmail.com>
2018-03-12 19:31:51 -0300

    swagger melhorado

--- DakiApp.webapi/Controllers/QuestionariosController.cs
+++ DakiApp.webapi/Controllers/QuestionariosController.cs
@@ -4,6 +4,7 @@ using DakiApp.domain.Contracts;
 using DakiApp.domain.Entities;
 using DakiApp.repository.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace DakiApp.webapi.Controllers
 {
@@ -24,88 +25,153 @@ namespace DakiApp.webapi.Controllers
         /// Lista todas os questionários cadastrados cadastrados
         /// </summary>
         /// <returns> Lista de questionários</returns>
-        /// <response code "200"> Retorna uma lista de curso</response>
+        /// <response code "200"> Retorna uma lista de cursos</response>
         /// /// <response code "400"> Ocorreu um erro</response>
         [HttpGet]
+        [ProducesResponseType(typeof(List<QuestionariosDomain>), 200)]
+        [ProducesResponseType(typeof(string), 400)]
         public IActionResult Listar()
         {
-            return Ok(_repo.Listar());
+            try
+            {
+                return Ok(_repo.Listar());
+            }
+            catch(System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         /// <summary>
         /// Lista dados do questionário requisitado
         /// </summary>
+        /// <param name="id">Id do questionário</param>
         /// <returns> Questionário de id indicado, com todas suas perguntas e todas as alternativas destas</returns>
         /// <response code "200"> Retorna uma questionário, com seus detalhes</response>
-        /// /// <response code "400"> Ocorreu um erro</response>
+        /// /<response code "400"> Ocorreu um erro</response>
+        /// <response code="404">Id não encontrado</response>
         [HttpGet ("{id}")]
+        // [ProducesResponseType(typeof(????), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        [ProducesResponseType(typeof(string), 400)]
         public IActionResult BuscarPorId(int id)
         {
-            var questionario =  _context
+            try
+            { 
+                var questionario =  _context
                 .Questionarios
                 .Include(d => d.QuestionarioPerguntas)
                 .ThenInclude(d => d.Pergunta.Alternativas)
                 .FirstOrDefault(d => d.id == id);
+                    if (questionario == null)
+                    {
+                        return NotFound("Id não encontrado");
+                    }
             
-            var respostaJson = new {
-                questionario.id,
-                questionario.Nome,
-                perguntas = questionario.QuestionarioPerguntas.Select(d => new {
-                    d.Pergunta.id,
-                    d.Pergunta.TipoResposta,
-                    d.Pergunta.Enunciado,
-                    Alternativas = d.Pergunta.Alternativas.Select(g => new {
-                        g.id,
-                        g.Conteudo,
-                        g.DataCriacao,
-                    }).ToArray(),
-                }).ToArray()
-            };
-            return Ok(respostaJson);
+                var respostaJson = new {
+                    questionario.id,
+                    questionario.Nome,
+                    perguntas = questionario.QuestionarioPerguntas.Select(d => new {
+                        d.Pergunta.id,
+                        d.Pergunta.TipoResposta,
+                        d.Pergunta.Enunciado,
+                        Alternativas = d.Pergunta.Alternativas.Select(g => new {
+                            g.id,
+                            g.Conteudo,
+                            g.DataCriacao,
+                        }).ToArray(),
+                    }).ToArray()
+                };
+                return Ok(respostaJson);
+            }
+            catch(System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
             // var includes = new string[]{ "QuestionarioPerguntas", "QuestionarioPerguntas.Pergunta.Alternativas"};
             // return Ok(_repo.BuscarPorId(id,includes));
         }
 
-        /// <summary>
-        /// Deleta questionário indicado
-        /// </summary>
-        /// <returns> ok </returns>
-        /// <response code "200"> Retorna uma lista de questionários</response>
-        /// /// <response code "400"> Ocorreu um erro</response>
-        [HttpDelete ("{id}")]
-        public IActionResult Deletar(int id)
-        {
-            var questionarios = _repo.BuscarPorId(id);
-            return Ok(_repo.Deletar(questionarios));
-        }
+        
 
         /// <summary>
         /// Cadastra novo questionário
         /// </summary>
+        /// <param name="QuestionarioDomain">Questionário</param>
         /// <returns> ok </returns>
-        /// <response code "200"> Retorna uma lista de quesionário</response>
-        /// /// <response code "400"> Ocorreu um erro</response>
+        /// <response code "200"> Retorna ok </response>
+        ///  <response code "400"> Ocorreu um erro</response>
         [HttpPost]
+        //[ProducesResponseType(typeof(), 200)]
+        [ProducesResponseType(typeof(string), 400)]
         public IActionResult Inserir([FromBody]QuestionariosDomain Questionarios)
         {
-            return Ok(_repo.Inserir(Questionarios));
+            try
+            {
+                return Ok(_repo.Inserir(Questionarios));
+            }
+            catch(System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }               
         }
 
         /// <summary>
         /// Atualiza o questionário indicado
         /// </summary>
+        /// <param name="id">Id do questionário</param>
         /// <returns> ok </returns>
         /// <response code "200"> Retorna uma lista de questionários</response>
-        /// /// <response code "400"> Ocorreu um erro</response>
+        /// <response code "400"> Ocorreu um erro</response>
+        /// <response code "404"> Id não encontrado</response>
         [HttpPut ("{id}")]
-        public IActionResult Autalizar(int id)
+        // [ProducesResponseType(typeof(), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(string), 404)]
+        public IActionResult Atualizar(int id)
         {
-            var questionarios = _repo.BuscarPorId(id);
-            return Ok(_repo.Atualizar(questionarios));
+            try
+            {
+                var questionarios = _repo.BuscarPorId(id);
+                if (questionarios == null)
+                {
+                    return NotFound("Id não encontrado");
+                }
+                return Ok(_repo.Atualizar(questionarios));
+            }
+            catch(System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-
-        
+        /// <summary>
+        /// Deleta questionário indicado
+        /// </summary>
+        /// <param name="id">Id do questionário</param>
+        /// <returns> ok </returns>
+        /// <response code "200"> Retorna Ok</response>
+        /// <response code "400"> Ocorreu um erro</response>
+        /// <response code "404"> Id não encontrado</response>
+        [HttpDelete ("{id}")]
+        //[ProducesResponseType(typeof(), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(string), 404)]
+        public IActionResult Deletar(int id)
+        {
+            try{
+                var questionarios = _repo.BuscarPorId(id);
+                if (questionarios == null){
+                    return NotFound("Id não encontrado");
+                }
+                return Ok(_repo.Deletar(questionarios));
+            }
+            catch(System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }   
+        }
     }
 }
