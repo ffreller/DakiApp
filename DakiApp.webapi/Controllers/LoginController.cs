cc89dc387ac3e792bfe82f754d630a6dd150eaaf
Fabio Freller <f.freller@gmail.com>
2018-03-28 19:23:27 -0300

    login adaptado para senha encriptada

--- DakiApp.webapi/Controllers/LoginController.cs
+++ DakiApp.webapi/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
+using System.Text;
 using DakiApp.domain.Contracts;
 using DakiApp.domain.Entities;
 using DakiApp.repository.Context;
@@ -29,51 +30,63 @@ namespace DakiApp.webapi.Controllers
         [HttpPost]
         public object Login([FromBody]UsuariosDomain usuario, [FromServices]signingConfigurations signingConfigurations, [FromServices]TokenConfigurations tokenConfigurations)
         {
-            UsuariosDomain user = contexto.Usuarios.Include("UsuarioPermissoes").Include("UsuarioPermissoes.Permissao").FirstOrDefault(c => c.Email == usuario.Email && c.Senha == usuario.Senha);
+            try{
+                HashPassword geradorHash = new HashPassword();
+                    var hash = geradorHash.GenerateHash(usuario, 10000, usuario.Senha.Length + usuario.DataCriacao.ToString().Length);
+                    if(hash != null){
+                        usuario.Senha = Encoding.ASCII.GetString(hash).ToString();
+                    }
+                //não sei se o bloco acima é necessário
+                UsuariosDomain user = contexto.Usuarios.Include("UsuarioPermissoes").Include("UsuarioPermissoes.Permissao").FirstOrDefault(c => c.Email == usuario.Email && c.Senha == usuario.Senha);
 
-            if(user != null)
-            {
-                ClaimsIdentity identity = new ClaimsIdentity(new GenericIdentity(user.id.ToString(), "Login"), new[] {
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
-                    new Claim(JwtRegisteredClaimNames.UniqueName, user.id.ToString()),
-                    new Claim("Nome", user.Nome),
-                    new Claim("Id", user.id.ToString()),
-                    new Claim(ClaimTypes.Email, user.Email)
-                });
-
-                var claims = new List<Claim>();
-                foreach(var item in user.UsuarioPermissoes)
+                if(user != null)
                 {
-                    claims.Add(new Claim(ClaimTypes.Role, item.Permissao.Nome));
+                    ClaimsIdentity identity = new ClaimsIdentity(new GenericIdentity(user.id.ToString(), "Login"), new[] {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                        new Claim(JwtRegisteredClaimNames.UniqueName, user.id.ToString()),
+                        new Claim("Nome", user.Nome),
+                        new Claim("Id", user.id.ToString()),
+                        new Claim(ClaimTypes.Email, user.Email)
+                    });
+
+                    var claims = new List<Claim>();
+                    foreach(var item in user.UsuarioPermissoes)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, item.Permissao.Nome));
+                    }
+                    
+                    identity.AddClaims(claims);
+                
+                    var handler = new JwtSecurityTokenHandler();
+                    var securityToken = handler.CreateToken(new SecurityTokenDescriptor{
+                        Issuer = tokenConfigurations.Issuer,
+                        Audience = tokenConfigurations.Audience,
+                        SigningCredentials = signingConfigurations.SigningCredentials,
+                        Subject = identity,
+                    });
+
+                    var token = handler.WriteToken(securityToken);
+
+                    var respostaJson = new {
+                        user.id,
+                        user.Nome,
+                        permissoes = user.UsuarioPermissoes.Select(d => new {
+                            d.Permissao.Nome
+                            }).ToArray()                  
+                    };
+                    
+                    var retorno = new{atutenticacao = true,  acessToken = token, message = "OK", usuario = respostaJson};
+
+                    return Ok(retorno);
                 }
-                
-                identity.AddClaims(claims);
-            
-                var handler = new JwtSecurityTokenHandler();
-                var securityToken = handler.CreateToken(new SecurityTokenDescriptor{
-                    Issuer = tokenConfigurations.Issuer,
-                    Audience = tokenConfigurations.Audience,
-                    SigningCredentials = signingConfigurations.SigningCredentials,
-                    Subject = identity,
-                });
 
-                var token = handler.WriteToken(securityToken);
-
-                var respostaJson = new {
-                    user.id,
-                    user.Nome,
-                    permissoes = user.UsuarioPermissoes.Select(d => new {
-                        d.Permissao.Nome
-                        }).ToArray()                  
-                };
-                
-                var retorno = new{atutenticacao = true,  acessToken = token, message = "OK", usuario = respostaJson};
-
-                return Ok(retorno);
+                var retornoerro = new {autenticacao = false, message = "Falha na Autenticação"};
+                return BadRequest(retornoerro);
             }
-
-            var retornoerro = new {autenticacao = false, message = "Falha na Autenticação"};
-            return BadRequest(retornoerro);
+            catch(System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [Route("api/logout")]
